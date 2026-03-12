c67a282cac0088373c744fb4c3717b12023dd9d3
Rameshmanohar18 <rameshbradmandgl@gmail.com>
2026-03-12 20:54:54 +0530

    Navbar changed

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,15 +1,37 @@
 import { Link } from "react-router-dom"
 
 export default function Navbar(){
-  return(
-    <nav className="bg-blue-600 text-white p-4 flex justify-between">
-      <h1 className="font-bold text-xl">🎓 College Portal</h1>
 
-      <div className="space-x-4">
-        <Link to="/">Home</Link>
-        <Link to="/dashboard">Dashboard</Link>
-        <Link to="/issues">Issues</Link>
-        <Link to="/report">Report Issue</Link>
+  return(
+    <nav className="bg-white shadow-md p-4">
+      <div className="max-w-6xl mx-auto flex justify-between items-center">
+
+        {/* Logo */}
+        <h1 className="text-xl font-bold text-blue-600">
+          Student Portal
+        </h1>
+
+        <div className="flex items-center gap-48">
+
+          <Link to="/" className="hover:text-blue-600">
+            Home
+          </Link>
+
+          <Link to="/dashboard" className="hover:text-blue-600">
+            Dashboard
+          </Link>
+
+          <Link to="/issues" className="hover:text-blue-600">
+            Issues
+          </Link>
+
+          <Link
+            to="/report"
+            className="bg-blue-600 text-white px-4 py-1 rounded"
+          >
+            Report Issue
+          </Link>
+        </div>
       </div>
     </nav>
   )
